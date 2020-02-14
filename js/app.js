0167be967260d1aaab413e0b4d7c17872337a263
87johnboyle <john.hazukisan@gmail.com>
2020-02-14 11:14:50 +0000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Note on console that JS has loaded, event listened for is the DOM content loading.
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+
+
+});
